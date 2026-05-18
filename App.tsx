615f6a42fb89a0fc7771150605ed77d0a7165cb8
BERNARDO RABELO MUNDO <mundo.0957@aluno.pr.senac.br>
2026-05-18 12:28:49 -0300

    Iniciando funcionalidades no App.tsx, colocando componentes, styles para começar a primeira tela

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView, StatusBar
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <View>
+          <Image />
+          <Text>McDonald's</Text>
+        </View>
+        <View>
+          <Text>McDonald's</Text>
+          <Text>Escolha como prefere aproveitar seu lanche. Estamos aqui para oferecer praticidade e sabor em cada detalhe.</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  safeArea: {
+
   },
-});
+  container: {
+
+  },
+  header: {
+
+  },
+  logo: {
+
+  },
+  brandName: {
+
+  },
+  welcomeSection: {
+
+  },
+  Subtitle: {
+
+  },
+  cardsRow: {
+
+  },
+  card: {
+
+  },
+  iconCircle: {
+
+  },
+  CardLabelPill: {
+
+  },
+  cardLabel: {
+
+  },
+})
